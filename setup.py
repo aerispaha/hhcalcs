61cc1f25f088808999bbce84dc3d073eb44a60ae
Adam Erispaha <Adam.Erispaha@water.gov>
2016-09-06 15:19:03 -0400

    bare package setup complete

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='hhcalcs',
       version='0.1',
       description='Tools for common hydraulic and hydrologic engineering calculations',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/aerispaha/hhcalcs',
       author='Adam Erispaha',
       author_email='aerispaha@gmail.com',
       license='MIT',
